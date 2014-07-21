eaee991fd1e4c49a1e7b53ae5e7b49249e994a4c
Jupotter <jupotter.g@gmail.com>
2014-07-21 20:46:07 +0200

    Create hazard unit class

---
+++ src/hazard_unit.cpp
@@ -0,0 +1,11 @@
+#include "hazard_unit.hh"
+
+HazardUnit::HazardUnit(Context& context)
+    : _context(context)
+{ }
+
+bool HazardUnit::checkHazard(Interstage& interstage)
+{
+
+    return false;
+}
